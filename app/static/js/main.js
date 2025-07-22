ad5ee746724b80a1c0bfb2dc416c96a7c751aab1
Axander Wilson <axanderwilson@Axanders-MacBook-Air.local>
2025-07-22 18:10:02 -0400

    mobile view feat fix.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,10 +3,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggleButton = document.getElementById("menu-toggle");
   const modal = document.getElementById("mobile-nav-modal");
+  const menuIcon = document.getElementById("menu-icon");
+  const closeIcon = document.getElementById("close-icon");
 
-  if (toggleButton && modal) {
+  if (toggleButton && modal && menuIcon && closeIcon) {
     toggleButton.addEventListener("click", () => {
       modal.classList.toggle("active");
+
+      const isActive = modal.classList.contains("active");
+      menuIcon.style.display = isActive ? "none" : "inline";
+      closeIcon.style.display = isActive ? "inline" : "none";
     });
   }
 });
